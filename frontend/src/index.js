ba2cabbea55c67b4f6ec2907e237065332fe7bd6
hopegipson <gipson.hope@gmail.com>
2021-03-30 16:27:52 -0500

    Added some basic reducer functionality, synced devtools, installed some packages

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import { Provider } from 'react-redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import musicReducer from './reducers/musicReducer.js';
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  musicReducer,
+  composeEnhancer(applyMiddleware(thunk)),
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+); 
+
---
+++ frontend/src/reducers/musicReducer.js
@@ -0,0 +1,20 @@
+const musicReducer = (state = { songs: [], loading: false }, action) => {
+    switch(action.type) {
+      case 'LOADING_SONGS':
+        return {
+         // ...state,
+         // cats: [...state.cats],
+         // loading: true
+        }
+      case 'ADD_SONGS':
+        return {
+         // ...state,
+         // cats: action.cats,
+        //  loading: false
+        }
+      default:
+        return state;
+    }
+  }
+  
+  export default musicReducer; 
